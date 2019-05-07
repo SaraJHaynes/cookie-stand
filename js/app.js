6c0049a7fd6e3824ad80358d8ee94d5cf3330bd9
Sara Haynes <sjhaynes@gmail.com>
2019-05-07 15:39:19 -0700

    working pike store

--- js/app.js
+++ js/app.js
@@ -1,152 +1,55 @@
-// name: 'First and Pike',
-    // minCustomers: 23,
-    // maxCustomers: 65,
-    // avgCookieSale: 6.3, 
-    // checkpikeStore: function(){
-    
-// }
-// function getRandomCostumer(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-//   }
+'use strict';
 
-//   function getRandomArbitrary(min, max) {
-//     return Math.random() * (max - min) + min;
-//   }
+//do this for all five stores
+var firstPike = document.getElementById('first-pike');
 
-// var PikeStore = []
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-
-
-// var pikeStore = [Minimum, Maximum, Sales]; {
-// var customersMin =[23];
-// var customersMax=[65];
-// var cookieSales=[6.3]; 
-// return[minCustomers[0], maxCustomers[0], avgCookieSale[0]];
-//     }
-
-
-//  for (var i = 0; i < totalStoreCount.length; i++) {
-// //   console.log(store[i].location)
-// totalStoreCount[i].render()
-
-
-
-//Javascript objects
-
-//Object literal notation (property:value,)
-
-var storelist = document.getElementById('storelist');
-// var firstListItem = storelist.firstChild.textContent;
-
-
+function calcRandomCustomers(min, max) {
+    // following line from MDN docs on Math.random
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 var pikeStore = {
   name: 'Pike Store',
   minCustomers: 23,
   maxCustomers: 65,
-  avgCookieSales: ['6.3'],
-  introduction: function(){
-    console.log('The name of this store is ' + this.name);
-  },
-  render: function() {
-    //create an element
+  avgCookieSales: 6.3,
+  //customersEachHour: [],
+  hourlySales: [],
+  totalCookiesForTheDay: 0,
+  hourlySalesCalc: function() {
+    for (var i = 0; i < hours.length; i++){
+    	this.hourlySales.push(Math.ceil(calcRandomCustomers(this.minCustomers, this.maxCustomers) * this.avgCookieSales));
+  	}
+	},
+	totalCookiesForTheDayCalc: function(){
+		for(var i = 0; i < hours.length; i++){
+			this.totalCookiesForTheDay += this.hourlySales[i];
+		}
+	},
+	render: function() {
+		for(var i = 0; i < hours.length; i++) {
+		 //create an element
     var liEl = document.createElement('li');
-    console.log('liEl is', liEl)
+    //console.log('liEl is', liEl)
     //give the element content
-    liEl.textContent = this.name;
-    console.log('liEL with content', liEl);
+    liEl.textContent = hours[i] + ': ' + this.hourlySales[i] + ' cookies.';
+    //console.log('liEL with content', liEl);
     //append the element to the dom
-    storelist.appendChild(liEl);
-  }
+    firstPike.appendChild(liEl);
+		}
+		liEl = document.createElement('li');
+		liEl.textContent = 'Total: ' + this.totalCookiesForTheDay;
+    firstPike.appendChild(liEl);
+	},
+	allCall: function() {
+		this.hourlySalesCalc();
+		this.totalCookiesForTheDayCalc();
+		this.render();
+		
+	}
 
-  };
+};
 
-  var seaTacStore = {
-    name: 'SeaTac Store',
-    minCustomers: 3,
-    maxCustomers: 24,
-    avgCookieSales: ['1.2'],
-    introduction: function(){
-      console.log('The name of this store is ' + this.name);
-    },
-    render: function() {
-      //create an element
-      var liEl = document.createElement('li');
-      console.log('liEl is', liEl)
-      //give the element content
-      liEl.textContent = this.name;
-      console.log('liEL with content', liEl);
-      //append the element to the dom
-      storelist.appendChild(liEl);
-    }
-  };
-  var seattleCenterStore = {
-    name: 'Seattle Center Store',
-    minCustomers: 11,
-    maxCustomers: 38,
-    avgCookieSales: ['3.7'],
-    introduction: function(){
-      console.log('The name of this store is ' + this.name);
-    },
-    render: function() {
-      //create an element
-      var liEl = document.createElement('li');
-      console.log('liEl is', liEl)
-      //give the element content
-      liEl.textContent = this.name;
-      console.log('liEL with content', liEl);
-      //append the element to the dom
-      storelist.appendChild(liEl);
-    }
-  };
-  var capitalHillStore = {
-    name: 'Capital Hill Store',
-    minCustomers: 20,
-    maxCustomers: 38,
-    avgCookieSales: ['2.3'],
-    introduction: function(){
-      console.log('The name of this store is ' + this.name);
-    },
-    render: function() {
-      //create an element
-      var liEl = document.createElement('li');
-      console.log('liEl is', liEl)
-      //give the element content
-      liEl.textContent = this.name;
-      console.log('liEL with content', liEl);
-      //append the element to the dom
-      storelist.appendChild(liEl);
-    }
-  };
-  var alkiStore = {
-    name: 'Alki Store',
-    minCustomers: 2,
-    maxCustomers: 16,
-    avgCookieSales: ['4.6'],
-    introduction: function(){
-      console.log('The name of this store is ' + this.name);
-    },
-    render: function() {
-      //create an element
-      var liEl = document.createElement('li');
-      console.log('liEl is', liEl)
-      //give the element content
-      liEl.textContent = this.name;
-      console.log('liEL with content', liEl);
-      //append the element to the dom
-      storelist.appendChild(liEl);
-    }
-  };
-var arrayOfStores = [pikeStore, seaTacStore, seattleCenterStore, capitalHillStore, alkiStore];
-for (var i = 0; i < arrayOfStores.length; i++)
-{
-    arrayOfStores[i].introduction();
-    arrayOfStores[i].render();
-}
-
-// var =[pikeStore, alki];
-
-//     for (var i = 0; i < stores.length; i++) {
-
-//     stores[i].render()
-// }
+pikeStore.allCall();
