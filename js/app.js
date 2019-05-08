a2399b2e2b7995d99d8d5a57e373f92ae66619d9
Sara Haynes <sjhaynes@gmail.com>
2019-05-08 16:07:30 -0700

    Header

--- js/app.js
+++ js/app.js
@@ -5,10 +5,11 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 var allStores = [];
 
 function calcRandomCustomers(min, max) {
-    // following line from MDN docs on Math.random
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  // following line from MDN docs on Math.random
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var storeTable = document.getElementById('storeTable');
+var storeTable = document.getElementById('store-table');
+// var tableTotal = document.getElementById('totalTable');
 
 // CONSTRUCTOR //
 function Store (name, min, max, avg) {
@@ -20,61 +21,84 @@ function Store (name, min, max, avg) {
   this.totalCookiesForTheDay = 0;
   this.hourlySalesCalc = function() {
     for (var i = 0; i < hours.length; i++){
-    	this.hourlySales.push(Math.ceil(calcRandomCustomers(this.minCustomers, this.maxCustomers) * this.avgCookieSales));
-  	}
-	};
-	this.totalCookiesForTheDayCalc = function(){
-		for(var i = 0; i < hours.length; i++){
-			this.totalCookiesForTheDay += this.hourlySales[i];
-		}
-	};
-	this.render = function () {
+      this.hourlySales.push(Math.ceil(calcRandomCustomers(this.minCustomers, this.maxCustomers) * this.avgCookieSales));
+    }
+  };
+  this.totalCookiesForTheDayCalc = function(){
+    for(var i = 0; i < hours.length; i++){
+      this.totalCookiesForTheDay += this.hourlySales[i];
+    }
+  };
+  this.render = function () {
     // make a tr
     var trEl = document.createElement('tr'); //this is the equivalent of typing <tr>
-		
-		// create, content, append for "Name"
-		var tdEl = document.createElement('td'); //equivalent of typing <td>
+
+    // create, content, append for "Name"
+    var tdEl = document.createElement('td'); //equivalent of typing <td>
     tdEl.textContent = this.name; //equivalent of: <td> "Text Content" </td>
-		trEl.appendChild(tdEl);
+    trEl.appendChild(tdEl);
 
-		for (var i = 0; i < hours.length; i++)
-		{ 
-			var tdEl = document.createElement('td'); //equivalent of typing <td>
-			tdEl.textContent = this.avgCookieSales; //equivalent of: <td> "Text Content" </td>
-			trEl.appendChild(tdEl);
-		}
-	
+    for (var i = 0; i < hours.length; i++)
+    {
+      tdEl = document.createElement('td'); //equivalent of typing <td>
+      tdEl.textContent = this.hourlySales[i]; //equivalent of: <td> "Text Content" </td>
+      trEl.appendChild(tdEl);
+    }
 
-		// //create the next element
-		// tdEl = document.createElement('td');
-		// tdEl.textContent = "5AM";
-		// trEl.appendChild(tdEl);
+    // //create the next element
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.totalCookiesForTheDay;
+    trEl.appendChild(tdEl);
 
-		// //create the next element
-		// tdEl = document.createElement('td');
-		// tdEl.textContent = ;
-		// trEl.appendChild(tdEl);
-    
     // append the tr to the table
     storeTable.appendChild(trEl);
   };
 
-	this.allCall = function() {
-		this.hourlySalesCalc();
-		this.totalCookiesForTheDayCalc();
-		this.render();
-		
-	}
-	allStores.push(this);
-};
+  this.allCall = function() {
+    this.hourlySalesCalc();
+    this.totalCookiesForTheDayCalc();
+    this.render();
 
-var pikeStore = new Store('First and Pike', 23, 65, 6.3);
-var seatacStore = new Store('SeaTac Airport', 3, 24, 1.2);
-var seattleCenterStore = new Store('Seattle Center', 11, 38, 3.7);
-var capitalHillStore = new Store('Capital Hill', 20, 38, 2.3);
-var alkiStore = new Store('Alki', 2, 16, 4.6);
-
-for (var i = 0; i < allStores.length; i++)
-{
-	allStores[i].allCall();
+  };
+  allStores.push(this);
 }
+
+function renderAll(){
+  for (var i = 0; i < allStores.length; i++){
+    allStores[i].allCall();
+  }
+}
+
+function tableHeader(){
+  var trEl = document.createElement('tr'); //this is the equivalent of typing <tr>
+
+  // create, content, append for "Name"
+  var thEl = document.createElement('th'); //equivalent of typing <td>
+  thEl.textContent = 'Stores'; //equivalent of: <td> "Text Content" </td>
+  trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++){
+    thEl = document.createElement('th'); //equivalent of typing <td>
+    thEl.textContent = hours[i]; //equivalent of: <td> "Text Content" </td>
+    trEl.appendChild(thEl);
+  }
+
+  // //create the next element
+  thEl = document.createElement('th');
+  thEl.textContent = 'Totals';
+  trEl.appendChild(thEl);
+
+  // append the tr to the table
+  storeTable.appendChild(trEl);
+
+}
+
+new Store('First and Pike', 23, 65, 6.3);
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capital Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
+
+tableHeader();
+renderAll();
+
